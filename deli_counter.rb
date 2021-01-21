7bc74bb58df5ac9647da63b13678e008745818e0
Vicky  Tee <vttak9@gmail.com>
2021-01-21 03:24:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 def line(katz_deli)
   if katz_deli == []
   puts "The line is currently empty."
- else 
+ else
    current_line="The line is currently:"
    katz_deli.each_with_index do |person, index|
      current_line << " #{index+1}. #{person}"
