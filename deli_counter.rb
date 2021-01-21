c243578cf2532191879aeb1003eb13d27f0bc54c
Vicky  Tee <vttak9@gmail.com>
2021-01-21 03:24:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,10 +23,10 @@ def line(katz_deli)
  end
 end
 
-def take_a_number(katz_deli, person)
-  katz_deli << person
-  puts "Welcome, #{person}. You are number #{katz_deli.size} in line."
-end
+#def take_a_number(katz_deli, person)
+#  katz_deli << person
+#  puts "Welcome, #{person}. You are number #{katz_deli.size} in line."
+#end
 
 def now_serving(katz_deli)
   if katz_deli == []
